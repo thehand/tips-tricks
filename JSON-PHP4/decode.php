db7942137ae6de769e0f9d161c641bdc26b4c5e6
Francesco Salamone <salamone.francesco@gmail.com>
2015-05-15 18:54:52 +0200

    Added Encode & Decode functions for JSON in the PHP4 language
    thanks to http://tinsology.net/2011/04/php-json_encode-and-json_decode-alternatives/

---
+++ JSON-PHP4/decode.php
@@ -0,0 +1,32 @@
+<?php
+
+if(!function_exists('json_decode'))
+{
+    function json_decode($json)
+    {
+        $comment = false;
+        $out = '$x=';
+        for ($i=0; $i<strlen($json); $i++)
+        {
+            if (!$comment)
+            {
+                if (($json[$i] == '{') || ($json[$i] == '['))
+                    $out .= ' array(';
+                else if (($json[$i] == '}') || ($json[$i] == ']'))
+                    $out .= ')';
+                else if ($json[$i] == ':')
+                    $out .= '=>';
+                else
+                    $out .= $json[$i];
+            }
+            else
+                $out .= $json[$i];
+            if ($json[$i] == '"' && $json[($i-1)]!="\\")
+                $comment = !$comment;
+        }
+        eval($out . ';');
+        return $x;
+    }
+}
+
+?>
