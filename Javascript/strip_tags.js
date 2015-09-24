02081b86655b12766cb322d2fc5abb7035a4fbaa
Francesco Salamone <salamone.francesco@gmail.com>
2015-09-24 23:06:47 +0200

    First working version

--- Javascript/strip_tags.js
+++ Javascript/strip_tags.js
@@ -1,10 +1,18 @@
-String.prototype.stripTags = function (allowed_tags) {
-  var allow = '';
-  if (allowed_tags != undefined && allowed_tags.length > 0) {
-    allow = "(!" + allowed_tags.join("|") + ")";
-  }
-  var reStr = '(<\/?([^' + allow + '>]+)\/?>)';
-  console.log(reStr);
+/**
+* @file strip_tags.js
+* @brief A colletion of useful missing functions in javascript
+* @author Francesco Salamone
+* @version 0.1
+* @date 2015-09-24
+*/
+
+/**
+* @name stripTags()
+* Stip all HTML tags from the passed string
+* TODO: I would like to choose the tags to strip and, if possible, what tags to allow
+**/
+String.prototype.stripTags = function () {
+  var reStr = '(<\\/?\\w+(?:[\\s\\w="\':;_\\/\\.\\-&#]*)\\/?>)';
   var re = new RegExp(reStr,'ig');
 
   return this.replace(re,"");
